8db3bbcaa5d2c18da486fba2984b0238f7080687
Andrés <andresglx@users.noreply.github.com>
2015-10-27 20:41:33 +0000

    Create my-first-wp-plugin.php
    This is the main file to  create a WP Plugin.

---
+++ my-first-wp-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: My First Plugin
+* Plugin URL: http://andresgl.es
+* Description: This is my first WP Plugin
+* Author: Andrés GL
+* Author URL: http://andresgl.es
+* Version: 1.0
+* License: GPLv2
+*/
+?>
